e5796d686a8c11e8d13a38848cb725b54ef695c0
CMorar143 <cian@morar.org>
2019-10-29 23:21:32 +0000

    testing circle hough transform

--- C16460726.py
+++ C16460726.py
@@ -9,23 +9,13 @@
 # import the necessary packages:
 import cv2
 import easygui as gui
-
-# Function to apply a threshold to an image
-# It is used to remove the background:
-def ApplyMask(threshold):
-	# Apply the mask to each color channel in the image:
-	extracted_sharkB = cv2.bitwise_or(threshold, Enchanced_B)
-	extracted_sharkG = cv2.bitwise_or(threshold, Enchanced_G)
-	extracted_sharkR = cv2.bitwise_or(threshold, Enchanced_R)
-
-	merged = cv2.merge((extracted_sharkB, extracted_sharkG, extracted_sharkR))
-
-	return merged
-
-# Repeat until the user chooses to exit:
+from matplotlib import pyplot as plt
+import numpy as np
 
 F = gui.fileopenbox()
-I = cv2.imread("./spottheball.jpg")
+# I = cv2.imread("./Images/spottheball.jpg")
+I = cv2.imread(F)
+output = I.copy()
 
 YUV = cv2.cvtColor(I, cv2.COLOR_BGR2YUV)
 Y, U, V = cv2.split(YUV)
@@ -33,14 +23,67 @@ Y, U, V = cv2.split(YUV)
 HSV = cv2.cvtColor(I, cv2.COLOR_BGR2HSV)
 H, S, HV = cv2.split(HSV)
 
+# Using the Contrast Limited Adaptive Histogram Equalization class to enhance the contrast
+# Create the CLAHE object and set the clip limit and tile grid size:
+CLAHE = cv2.createCLAHE(clipLimit = 4.5, tileGridSize = (3,3))
+
+# This improves definition in the image:
+Enhanced_Y = CLAHE.apply(Y)
+E = cv2.equalizeHist(Y)
+
+_, Threshold = cv2.threshold(E, 248, 255, cv2.THRESH_BINARY)
+G = cv2.cvtColor(I, cv2.COLOR_BGR2GRAY)
+G = cv2.medianBlur(G, 15)
+
+circles = cv2.HoughCircles(G, cv2.HOUGH_GRADIENT, 1, 50, param1=50, param2=30, minRadius=0, maxRadius=0)
+
+cv2.imshow("Threshold", Threshold)
+
+detected = np.uint16(np.around(circles))
+
+for (x, y, r) in detected[0, :]:
+	cv2.circle(output, (x, y), 2, (0, 0, 255), 3)
+	cv2.circle(output, (x, y), r, (255, 0, 0), 3)
+
+# cv2.imshow("G", G)
+cv2.imshow("output", output)
+cv2.waitKey(0)
 
 
 
 
+################# Plot Diagram #################
 
+fig = plt.figure()
+gs = fig.add_gridspec(3, 2)
 
+ax1 = fig.add_subplot(gs[0,0])
+ax1.imshow(Y, cmap='gray')
 
+ax2 = fig.add_subplot(gs[0,1])
+ax2.hist(Y.ravel(), bins=256, range=[0,256])
 
+ax3 = fig.add_subplot(gs[1,0])
+ax3.imshow(E, cmap='gray')
+
+ax4 = fig.add_subplot(gs[1,1])
+ax4.hist(E.ravel(), bins=256, range=[0,256])
+
+ax5 = fig.add_subplot(gs[2,0])
+ax5.imshow(Enhanced_Y, cmap='gray')
+
+ax6 = fig.add_subplot(gs[2,1])
+ax6.hist(Enhanced_Y.ravel(), bins=256, range=[0,256])
+
+# ax7 = fig.add_subplot(gs[3,0])
+# ax7.imshow(gray, cmap='gray')
+
+# ax8 = fig.add_subplot(gs[3,1])
+# ax8.hist(gray.ravel(), bins=256, range=[0,256])
+
+# plt.show()
+# cv2.waitKey(0)
+# plt.close()
 
 
 
