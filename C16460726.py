3c37d62fc24060b55da7cc4ae6cfce8b39a64f8e
CMorar143 <cian@morar.org>
2019-11-16 03:33:37 +0000

    CLEANING UP CODE

--- C16460726.py
+++ C16460726.py
@@ -17,9 +17,9 @@ def FindGrass(xCircle, yCircle, yStart, yFinish, xStart, xFinish):
 	# Extract grass from the right hand side
 	Extracted_grass = np.zeros(shape=[h, w, 3], dtype=np.uint8)
 	cv2.circle(Extracted_grass, (xCircle, yCircle), r, (255, 255, 255), -1)
-	grass_extraction = cv2.subtract(output, Extracted_grass)
+	grass_extraction = cv2.subtract(I, Extracted_grass)
 	Extracted_grass = cv2.bitwise_not(Extracted_grass)
-	grass_extraction2 = cv2.subtract(output, Extracted_grass)
+	grass_extraction2 = cv2.subtract(I, Extracted_grass)
 
 	# Zoom in and get average pixel value of extracted area
 	cropped_grass = grass_extraction2[yStart:yFinish, xStart:xFinish]
@@ -38,28 +38,12 @@ def FillEmptySpot(rx, ry, grass_extraction):
 
 
 # F = gui.fileopenbox()
-I = cv2.imread("./Images/spottheball.jpg")
+I = cv2.imread("./Images/snooker.jpg")
 # I = cv2.imread(F)
-output = I.copy()
 h, w, d = I.shape
 
 Extracted_ball = np.zeros(shape=[h, w, 3], dtype=np.uint8)
-
-YUV = cv2.cvtColor(I, cv2.COLOR_BGR2YUV)
-Y, U, V = cv2.split(YUV)
-
-HSV = cv2.cvtColor(I, cv2.COLOR_BGR2HSV)
-H, S, HV = cv2.split(HSV)
-
-# Using the Contrast Limited Adaptive Histogram Equalization class to enhance the contrast
-# Create the CLAHE object and set the clip limit and tile grid size:
-CLAHE = cv2.createCLAHE(clipLimit = 4.5, tileGridSize = (3,3))
-
-# This improves definition in the image:
-Enhanced_Y = CLAHE.apply(Y)
-E = cv2.equalizeHist(Y)
-
-_, Threshold = cv2.threshold(E, 248, 255, cv2.THRESH_BINARY)
+Extracted_grass = np.zeros(shape=[h, w, 3], dtype=np.uint8)
 G = cv2.cvtColor(I, cv2.COLOR_BGR2GRAY)
 G = cv2.medianBlur(G, 17)
 
@@ -67,14 +51,14 @@ circles = cv2.HoughCircles(G, cv2.HOUGH_GRADIENT, 1, I.shape[0], param1=50, para
 
 detected = np.uint16(np.around(circles))
 
-# Increase the radius by 3 to go beyond the circumference
+# Increase the radius by 10 to go beyond the circumference
 for (x, y, r) in detected[0, :]:
 	r = r + 10
 	cv2.circle(Extracted_ball, (x, y), (r), (255, 255, 255), -1)
 
-Test_extraction = cv2.subtract(output, Extracted_ball)
+Test_extraction = cv2.subtract(I, Extracted_ball)
 Extracted_ball = cv2.bitwise_not(Extracted_ball)
-Test_extraction2 = cv2.subtract(output, Extracted_ball)
+Extracted_ball = cv2.subtract(I, Extracted_ball)
 
 # Extract grass from the right hand side
 meanRight, grass_extraction2 = FindGrass(x + (r*2), y, y-r, y+r, x+r, x + (r*3))
@@ -92,7 +76,7 @@ print("left: ", meanLeft)
 meanTop, grass_extraction2T = FindGrass(x, y - (r*2), y-(r*3), y-r, x-r, x+r)
 print("top: ", meanTop)
 
-# Find which sidehas the highest amount of green
+# Find which side has the highest amount of green
 means = (meanRight, meanBottom, meanLeft, meanTop)
 highestMean = max(means)
 
@@ -108,59 +92,19 @@ if meanLeft == highestMean:
 if meanTop == highestMean:
 	dst = FillEmptySpot(0, r*2, grass_extraction2T)
 
-
-# cv2.imshow("G", G)
-# cv2.imshow("Threshold", Threshold)
-# cv2.imshow("Test_extraction", Test_extraction)
+cv2.imshow("Test_extraction", Test_extraction)
 
 # cv2.imshow("grass_extraction", grass_extraction)
 # cv2.imshow("grass_extraction2", grass_extraction2)
 # cv2.imshow("grass_extraction2B", grass_extraction2B)
 # cv2.imshow("grass_extraction2L", grass_extraction2L)
 # cv2.imshow("grass_extraction2T", grass_extraction2T)
-# cv2.imshow("Test_extraction2", Test_extraction2)
-# cv2.imshow("output", output)
+# cv2.imshow("output2", output2)
 cv2.imshow("dst", dst)
 # cv2.imshow("cropped_grass", cropped_grass)
 # cv2.imshow("cropped_grassB", cropped_grassB)
 # cv2.imshow("cropped_grassL", cropped_grassL)
 # cv2.imshow("cropped_grassT", cropped_grassT)
-# cv2.imshow("Extracted_ball", Extracted_ball)
-# cv2.imshow("Extracted_grass", Extracted_grass)
+cv2.imshow("Extracted_ball", Extracted_ball)
+cv2.imshow("Extracted_grass", Extracted_grass)
 cv2.waitKey(0)
-
-
-
-
-################# Plot Diagram #################
-
-fig = plt.figure()
-gs = fig.add_gridspec(3, 2)
-
-ax1 = fig.add_subplot(gs[0,0])
-ax1.imshow(Y, cmap='gray')
-
-ax2 = fig.add_subplot(gs[0,1])
-ax2.hist(Y.ravel(), bins=256, range=[0,256])
-
-ax3 = fig.add_subplot(gs[1,0])
-ax3.imshow(E, cmap='gray')
-
-ax4 = fig.add_subplot(gs[1,1])
-ax4.hist(E.ravel(), bins=256, range=[0,256])
-
-ax5 = fig.add_subplot(gs[2,0])
-ax5.imshow(Enhanced_Y, cmap='gray')
-
-ax6 = fig.add_subplot(gs[2,1])
-ax6.hist(Enhanced_Y.ravel(), bins=256, range=[0,256])
-
-# ax7 = fig.add_subplot(gs[3,0])
-# ax7.imshow(gray, cmap='gray')
-
-# ax8 = fig.add_subplot(gs[3,1])
-# ax8.hist(gray.ravel(), bins=256, range=[0,256])
-
-# plt.show()
-# cv2.waitKey(0)
-# plt.close()
